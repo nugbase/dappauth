ab6a5467903d5a01f1a47fe71f6c2c57a44a782b
Andrei Edell <andrei@nugbase.com>
2021-01-12 21:45:06 -0800

    ABI fixes for latest go-ethereum

--- ERCs/ERC1271.go
+++ ERCs/ERC1271.go
@@ -26,107 +26,107 @@ var (
 	_ = event.NewSubscription
 )
 
-// ERC1271ABI is the input ABI used to generate the binding from.
-const ERC1271ABI = "[{\"constant\":true,\"inputs\":[{\"name\":\"hash\",\"type\":\"bytes32\"},{\"name\":\"_signature\",\"type\":\"bytes\"}],\"name\":\"isValidSignature\",\"outputs\":[{\"name\":\"magicValue\",\"type\":\"bytes4\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
+// ERCsABI is the input ABI used to generate the binding from.
+const ERCsABI = "[{\"constant\":true,\"inputs\":[{\"name\":\"hash\",\"type\":\"bytes32\"},{\"name\":\"_signature\",\"type\":\"bytes\"}],\"name\":\"isValidSignature\",\"outputs\":[{\"name\":\"magicValue\",\"type\":\"bytes4\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
 
-// ERC1271 is an auto generated Go binding around an Ethereum contract.
-type ERC1271 struct {
-	ERC1271Caller     // Read-only binding to the contract
-	ERC1271Transactor // Write-only binding to the contract
-	ERC1271Filterer   // Log filterer for contract events
+// ERCs is an auto generated Go binding around an Ethereum contract.
+type ERCs struct {
+	ERCsCaller     // Read-only binding to the contract
+	ERCsTransactor // Write-only binding to the contract
+	ERCsFilterer   // Log filterer for contract events
 }
 
-// ERC1271Caller is an auto generated read-only Go binding around an Ethereum contract.
-type ERC1271Caller struct {
+// ERCsCaller is an auto generated read-only Go binding around an Ethereum contract.
+type ERCsCaller struct {
 	contract *bind.BoundContract // Generic contract wrapper for the low level calls
 }
 
-// ERC1271Transactor is an auto generated write-only Go binding around an Ethereum contract.
-type ERC1271Transactor struct {
+// ERCsTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type ERCsTransactor struct {
 	contract *bind.BoundContract // Generic contract wrapper for the low level calls
 }
 
-// ERC1271Filterer is an auto generated log filtering Go binding around an Ethereum contract events.
-type ERC1271Filterer struct {
+// ERCsFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type ERCsFilterer struct {
 	contract *bind.BoundContract // Generic contract wrapper for the low level calls
 }
 
-// ERC1271Session is an auto generated Go binding around an Ethereum contract,
+// ERCsSession is an auto generated Go binding around an Ethereum contract,
 // with pre-set call and transact options.
-type ERC1271Session struct {
-	Contract     *ERC1271          // Generic contract binding to set the session for
+type ERCsSession struct {
+	Contract     *ERCs             // Generic contract binding to set the session for
 	CallOpts     bind.CallOpts     // Call options to use throughout this session
 	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
 }
 
-// ERC1271CallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// ERCsCallerSession is an auto generated read-only Go binding around an Ethereum contract,
 // with pre-set call options.
-type ERC1271CallerSession struct {
-	Contract *ERC1271Caller // Generic contract caller binding to set the session for
-	CallOpts bind.CallOpts  // Call options to use throughout this session
+type ERCsCallerSession struct {
+	Contract *ERCsCaller   // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts // Call options to use throughout this session
 }
 
-// ERC1271TransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// ERCsTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
 // with pre-set transact options.
-type ERC1271TransactorSession struct {
-	Contract     *ERC1271Transactor // Generic contract transactor binding to set the session for
-	TransactOpts bind.TransactOpts  // Transaction auth options to use throughout this session
+type ERCsTransactorSession struct {
+	Contract     *ERCsTransactor   // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
 }
 
-// ERC1271Raw is an auto generated low-level Go binding around an Ethereum contract.
-type ERC1271Raw struct {
-	Contract *ERC1271 // Generic contract binding to access the raw methods on
+// ERCsRaw is an auto generated low-level Go binding around an Ethereum contract.
+type ERCsRaw struct {
+	Contract *ERCs // Generic contract binding to access the raw methods on
 }
 
-// ERC1271CallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
-type ERC1271CallerRaw struct {
-	Contract *ERC1271Caller // Generic read-only contract binding to access the raw methods on
+// ERCsCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type ERCsCallerRaw struct {
+	Contract *ERCsCaller // Generic read-only contract binding to access the raw methods on
 }
 
-// ERC1271TransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
-type ERC1271TransactorRaw struct {
-	Contract *ERC1271Transactor // Generic write-only contract binding to access the raw methods on
+// ERCsTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type ERCsTransactorRaw struct {
+	Contract *ERCsTransactor // Generic write-only contract binding to access the raw methods on
 }
 
-// NewERC1271 creates a new instance of ERC1271, bound to a specific deployed contract.
-func NewERC1271(address common.Address, backend bind.ContractBackend) (*ERC1271, error) {
-	contract, err := bindERC1271(address, backend, backend, backend)
+// NewERCs creates a new instance of ERCs, bound to a specific deployed contract.
+func NewERCs(address common.Address, backend bind.ContractBackend) (*ERCs, error) {
+	contract, err := bindERCs(address, backend, backend, backend)
 	if err != nil {
 		return nil, err
 	}
-	return &ERC1271{ERC1271Caller: ERC1271Caller{contract: contract}, ERC1271Transactor: ERC1271Transactor{contract: contract}, ERC1271Filterer: ERC1271Filterer{contract: contract}}, nil
+	return &ERCs{ERCsCaller: ERCsCaller{contract: contract}, ERCsTransactor: ERCsTransactor{contract: contract}, ERCsFilterer: ERCsFilterer{contract: contract}}, nil
 }
 
-// NewERC1271Caller creates a new read-only instance of ERC1271, bound to a specific deployed contract.
-func NewERC1271Caller(address common.Address, caller bind.ContractCaller) (*ERC1271Caller, error) {
-	contract, err := bindERC1271(address, caller, nil, nil)
+// NewERCsCaller creates a new read-only instance of ERCs, bound to a specific deployed contract.
+func NewERCsCaller(address common.Address, caller bind.ContractCaller) (*ERCsCaller, error) {
+	contract, err := bindERCs(address, caller, nil, nil)
 	if err != nil {
 		return nil, err
 	}
-	return &ERC1271Caller{contract: contract}, nil
+	return &ERCsCaller{contract: contract}, nil
 }
 
-// NewERC1271Transactor creates a new write-only instance of ERC1271, bound to a specific deployed contract.
-func NewERC1271Transactor(address common.Address, transactor bind.ContractTransactor) (*ERC1271Transactor, error) {
-	contract, err := bindERC1271(address, nil, transactor, nil)
+// NewERCsTransactor creates a new write-only instance of ERCs, bound to a specific deployed contract.
+func NewERCsTransactor(address common.Address, transactor bind.ContractTransactor) (*ERCsTransactor, error) {
+	contract, err := bindERCs(address, nil, transactor, nil)
 	if err != nil {
 		return nil, err
 	}
-	return &ERC1271Transactor{contract: contract}, nil
+	return &ERCsTransactor{contract: contract}, nil
 }
 
-// NewERC1271Filterer creates a new log filterer instance of ERC1271, bound to a specific deployed contract.
-func NewERC1271Filterer(address common.Address, filterer bind.ContractFilterer) (*ERC1271Filterer, error) {
-	contract, err := bindERC1271(address, nil, nil, filterer)
+// NewERCsFilterer creates a new log filterer instance of ERCs, bound to a specific deployed contract.
+func NewERCsFilterer(address common.Address, filterer bind.ContractFilterer) (*ERCsFilterer, error) {
+	contract, err := bindERCs(address, nil, nil, filterer)
 	if err != nil {
 		return nil, err
 	}
-	return &ERC1271Filterer{contract: contract}, nil
+	return &ERCsFilterer{contract: contract}, nil
 }
 
-// bindERC1271 binds a generic wrapper to an already deployed contract.
-func bindERC1271(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(ERC1271ABI))
+// bindERCs binds a generic wrapper to an already deployed contract.
+func bindERCs(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(ERCsABI))
 	if err != nil {
 		return nil, err
 	}
@@ -137,62 +137,67 @@ func bindERC1271(address common.Address, caller bind.ContractCaller, transactor 
 // sets the output to result. The result type might be a single field for simple
 // returns, a slice of interfaces for anonymous returns and a struct for named
 // returns.
-func (_ERC1271 *ERC1271Raw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
-	return _ERC1271.Contract.ERC1271Caller.contract.Call(opts, result, method, params...)
+func (_ERCs *ERCsRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _ERCs.Contract.ERCsCaller.contract.Call(opts, result, method, params...)
 }
 
 // Transfer initiates a plain transaction to move funds to the contract, calling
 // its default method if one is available.
-func (_ERC1271 *ERC1271Raw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
-	return _ERC1271.Contract.ERC1271Transactor.contract.Transfer(opts)
+func (_ERCs *ERCsRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _ERCs.Contract.ERCsTransactor.contract.Transfer(opts)
 }
 
 // Transact invokes the (paid) contract method with params as input values.
-func (_ERC1271 *ERC1271Raw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
-	return _ERC1271.Contract.ERC1271Transactor.contract.Transact(opts, method, params...)
+func (_ERCs *ERCsRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _ERCs.Contract.ERCsTransactor.contract.Transact(opts, method, params...)
 }
 
 // Call invokes the (constant) contract method with params as input values and
 // sets the output to result. The result type might be a single field for simple
 // returns, a slice of interfaces for anonymous returns and a struct for named
 // returns.
-func (_ERC1271 *ERC1271CallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
-	return _ERC1271.Contract.contract.Call(opts, result, method, params...)
+func (_ERCs *ERCsCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _ERCs.Contract.contract.Call(opts, result, method, params...)
 }
 
 // Transfer initiates a plain transaction to move funds to the contract, calling
 // its default method if one is available.
-func (_ERC1271 *ERC1271TransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
-	return _ERC1271.Contract.contract.Transfer(opts)
+func (_ERCs *ERCsTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _ERCs.Contract.contract.Transfer(opts)
 }
 
 // Transact invokes the (paid) contract method with params as input values.
-func (_ERC1271 *ERC1271TransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
-	return _ERC1271.Contract.contract.Transact(opts, method, params...)
+func (_ERCs *ERCsTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _ERCs.Contract.contract.Transact(opts, method, params...)
 }
 
 // IsValidSignature is a free data retrieval call binding the contract method 0x1626ba7e.
 //
-// Solidity: function isValidSignature(hash bytes32, _signature bytes) constant returns(magicValue bytes4)
-func (_ERC1271 *ERC1271Caller) IsValidSignature(opts *bind.CallOpts, hash [32]byte, _signature []byte) ([4]byte, error) {
-	var (
-		ret0 = new([4]byte)
-	)
-	out := ret0
-	err := _ERC1271.contract.Call(opts, out, "isValidSignature", hash, _signature)
-	return *ret0, err
+// Solidity: function isValidSignature(bytes32 hash, bytes _signature) view returns(bytes4 magicValue)
+func (_ERCs *ERCsCaller) IsValidSignature(opts *bind.CallOpts, hash [32]byte, _signature []byte) ([4]byte, error) {
+	var out []interface{}
+	err := _ERCs.contract.Call(opts, &out, "isValidSignature", hash, _signature)
+
+	if err != nil {
+		return *new([4]byte), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new([4]byte)).(*[4]byte)
+
+	return out0, err
+
 }
 
 // IsValidSignature is a free data retrieval call binding the contract method 0x1626ba7e.
 //
-// Solidity: function isValidSignature(hash bytes32, _signature bytes) constant returns(magicValue bytes4)
-func (_ERC1271 *ERC1271Session) IsValidSignature(hash [32]byte, _signature []byte) ([4]byte, error) {
-	return _ERC1271.Contract.IsValidSignature(&_ERC1271.CallOpts, hash, _signature)
+// Solidity: function isValidSignature(bytes32 hash, bytes _signature) view returns(bytes4 magicValue)
+func (_ERCs *ERCsSession) IsValidSignature(hash [32]byte, _signature []byte) ([4]byte, error) {
+	return _ERCs.Contract.IsValidSignature(&_ERCs.CallOpts, hash, _signature)
 }
 
 // IsValidSignature is a free data retrieval call binding the contract method 0x1626ba7e.
 //
-// Solidity: function isValidSignature(hash bytes32, _signature bytes) constant returns(magicValue bytes4)
-func (_ERC1271 *ERC1271CallerSession) IsValidSignature(hash [32]byte, _signature []byte) ([4]byte, error) {
-	return _ERC1271.Contract.IsValidSignature(&_ERC1271.CallOpts, hash, _signature)
+// Solidity: function isValidSignature(bytes32 hash, bytes _signature) view returns(bytes4 magicValue)
+func (_ERCs *ERCsCallerSession) IsValidSignature(hash [32]byte, _signature []byte) ([4]byte, error) {
+	return _ERCs.Contract.IsValidSignature(&_ERCs.CallOpts, hash, _signature)
 }
